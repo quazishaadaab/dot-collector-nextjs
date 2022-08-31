760be6140dcef323c45a57769582c3d08030be99
Quazi Shaadaab <quazishaadaab@Quazis-MacBook-Air.local>
2022-08-30 21:15:13 -0400

    changed http server url

--- utils/deployments.js
+++ utils/deployments.js
@@ -1,12 +1,14 @@
 
 
 //https://salty-tor-00815.herokuapp.com
-const BASE_BACKEND = "http://localhost:8000"
+// http://localhost:8000
+const BASE_BACKEND = "https://salty-tor-00815.herokuapp.com"
 
 
 
 //https://sleepy-dawn-45361.herokuapp.com
-const CANVAS_BACKEND = "http://localhost:2000"
+// http://localhost:2000
+const CANVAS_BACKEND = "https://sleepy-dawn-45361.herokuapp.com"
 
 const FRONT_END = "https://dot-collector-nextjs.vercel.app/"
 export {BASE_BACKEND}
