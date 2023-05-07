f80db2bc5d1ca0b1105e9aea4f93164ae09bc767
Quazi Shaadaab <quazishaadaab@Quazis-MacBook-Air.local>
2023-05-07 18:17:49 -0400

    remixing2

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ import Image from 'next/image'
 import React from "react";
 import { useEffect, useState } from "react";
 
-//import Home from "./home/[home]";
-//import Login from "./login/Login"
+import Home from "./home/[home]";
+import Login from "./login/Login"
 // import Grids from "./Grids.js"
 // import ScriptTag from 'react-script-tag';
 // import Test from "./Test.js"
@@ -17,16 +17,16 @@ import { useEffect, useState } from "react";
 // import './components/src/sketch.js';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import Attributes from "./attributes/Attributes";
+import Attributes from "./attributes/Attributes";
 
-//import Rooms from "./rooms/[room]";
+import Rooms from "./rooms/[room]";
 import { v4 as uuid } from "uuid";
 import { useDispatch, useSelector } from "react-redux";
 // import Search from "../components/search/Search"
 
 
 import { SessionProvider } from "next-auth/react";
-//import {payload} from "./home/[home]"
+import {payload} from "./home/[home]"
 import Landing from './Landing';
 import { getSession, signIn, signOut, useSession } from "next-auth/react";
 
@@ -43,7 +43,7 @@ const App: NextPage = () => {
   // must be named after the slice name( const {user} has to be the same name as slice name)
   //very important to add the question mark here. if we dont, then a bug will occur since the question mark verifies if the user object exits or not before accessing the userid label
 
-  //let userid = payload?.userid;
+  let userid = payload?.userid;
 
   // useEffect(() => {
   //  setUserData(userid);
@@ -54,11 +54,51 @@ const App: NextPage = () => {
                 href={`/api/auth/signin`}
                 onClick={(e) => {
                   e.preventDefault()
-                  signIn("google")
+                  signIn()
                 }}
               >
                 Sign in
               </a>
+         {!userid ? (
+          
+         <Landing/>
+    
+         ) : ( 
+          <>
+          <Home userid={userData} /> 
+
+         
+      {/* <Home userid={userid}/> */}
+          {/* <Home userid={userid} /> */}
+            {/* <Routes> */}
+              {/* <Route path="/">
+                <Route index element={<Home userid={userid} />}/>
+                <Route path="login" element={<Login />} />
+                <Route path=":id" element={<Home userid={userid} />} /> */}
+
+                {/* <Route path="users">
+                  <Route index element={<List />} />
+                  <Route path=":userId" element={<List />} />
+                </Route> */}
+
+                {/* <Route path="products">
+                  <Route index element={<List />} /> */}
+                  {/* <Route path="new" element={<Search />} /> */}
+                {/* </Route> */}
+
+                {/* <Route path="rooms">
+                  <Route path=":roomId" element={<Rooms unique_id={uid} />} />
+                </Route>
+              </Route>
+            </Routes> */}
+
+
+
+
+          </>
+        )} 
+
+
 
 
     </>
