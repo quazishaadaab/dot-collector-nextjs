e5d017dc08055190b2a7a1330dae277c5c036f07
Quazi Shaadaab <quazishaadaab@Quazis-MacBook-Air.local>
2023-05-07 18:09:13 -0400

    changed persist git wrapping

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,17 +7,23 @@ import {store} from "../services/redux/store";
 import {PersistGate} from 'redux-persist/integration/react'
 import {persistStore} from "redux-persist"
 
+let persistor = persistStore(store)
 
 // ... (triple dots) mean there are other things in it
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
   return (
     // need to wrap apollo for graphql to work( apollo connect our graphql endpoint)
+
       <SessionProvider session={session}>
+        <Provider store={store}>
+        <PersistGate loading ={null} persistor={persistor}>
              {/* makes whole app scrollable and grey */}
         <div className="h-screen overflow-y-scroll ">
 
           <Component {...pageProps} />
         </div>
+        </PersistGate>
+        </Provider> 
       </SessionProvider>
 
   );
