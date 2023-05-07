c85e4bae70d4e9bd1236887c23089a80001ed41b
Quazi Shaadaab <quazishaadaab@Quazis-MacBook-Air.local>
2023-05-07 14:20:39 -0400

    changed app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,20 +13,27 @@ let persistor = persistStore(store)
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
   return (
     // need to wrap apollo for graphql to work( apollo connect our graphql endpoint)
-<Provider store={store}>
-<PersistGate loading ={null} persistor={persistor}>
-      <SessionProvider session={session}>
+
+
+<SessionProvider session={session}>
              {/* makes whole app scrollable and grey */}
         <div className="h-screen overflow-y-scroll ">
 
           <Component {...pageProps} />
         </div>
       </SessionProvider>
-      </PersistGate>
-      </Provider>
-
   );
 }
 
 export default MyApp;
 
+{/* <Provider store={store}>
+<PersistGate loading ={null} persistor={persistor}>
+<SessionProvider session={session}> */}
+{/* makes whole app scrollable and grey */}
+{/* <div className="h-screen overflow-y-scroll "> */}
+{/* <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+      </PersistGate>
+      </Provider> */}
