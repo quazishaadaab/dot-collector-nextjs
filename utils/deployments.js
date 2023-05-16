fed3e1a08a3e14b3f1f62d279ee6ab0bf4700f22
Quazi Shaadaab <quazishaadaab@Quazis-MacBook-Air.local>
2023-05-15 20:37:09 -0400

    made url dots-prod-tau.vercel.app

--- utils/deployments.js
+++ utils/deployments.js
@@ -14,7 +14,7 @@ const CANVAS_BACKEND = "https://backend-static-canvas.fly.dev"
 // http://localhost:3000
 //https://dot-collector-nextjs.vercel.app/
 // const FRONT_END = "https://dotcollector.ca"
-const FRONT_END = "http://localhost:3000"
+const FRONT_END = "https://dots-prod-tau.vercel.app"
 export {BASE_BACKEND}
 export {CANVAS_BACKEND}
 export {FRONT_END}
